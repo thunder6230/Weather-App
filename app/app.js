c17b5f525c48610afc88f98c786d42ed1b1158ec
Andras Margittai <thunder6230@gmail.com>
2020-05-10 01:33:33 +0200

    weather-app

---
+++ app/app.js
@@ -0,0 +1,174 @@
+//Weather UI Elements
+const locationW = document.querySelector('#w-location')
+const description = document.querySelector('#w-desc')
+const wstring = document.querySelector('#w-string')
+const icon = document.querySelector('#w-icon')
+
+const humidity = document.querySelector('#w-humidity')
+const pressure = document.querySelector('#w-pressure')
+const feelsLike = document.querySelector('#w-feels-like')
+const wind = document.querySelector('#w-wind')
+
+//HTML Elements
+const saveBtn = document.querySelector('#w-change-btn')
+const flags = document.querySelectorAll('.flags img')
+const settingsBtn = document.querySelector('#settings')
+const closeBtn = document.querySelector('#close')
+
+
+//Default Setting
+getWeatherDefault()
+
+//New Location
+saveBtn.addEventListener('click', saveSettings)
+flags.forEach(img => {
+    img.addEventListener('click', setLang)
+});
+
+
+
+
+
+function saveSettings(){
+    const lang = document.querySelector('.active').id
+    const inputCity = document.querySelector('#city')
+    console.log(inputCity)
+    if(inputCity.value === ''){
+        switch(lang){
+            case 'en':
+                inputCity.value = `Please add a location!`
+                break
+            case 'hu':
+                inputCity.value = `Kerem adjon meg egy helyet!`
+                break
+            case 'de':
+                inputCity.value = `Bitte geben Sie einen Ort ein!`
+                break 
+        }          
+        inputCity.style.background = 'red'
+        inputCity.style.color = 'white'
+        inputCity.style.fontWeight = 'bold'
+        setTimeout(function(){
+            inputCity.value=""
+            inputCity.style.background = 'white'
+            inputCity.style.color = 'black'
+            inputCity.style.fontWeight = 'normal'
+        }, 2000)
+    }
+    getNewWeather()
+}
+
+
+
+
+function setLang(){
+    
+    console.log(event.target)
+    document.querySelector('.active').className = "none"
+    event.target.className = "active"
+    const lang = document.querySelector('.active').id
+    switch(lang){
+        case 'en':
+        saveBtn.textContent = "Save changes"
+        settingsBtn.textContent = "-->Change Settings"
+        closeBtn.textContent = "Close"
+        break
+        case 'hu':
+        saveBtn.textContent = "Mentes"
+        settingsBtn.textContent = "-->Beallitasok"
+        closeBtn.textContent = "Bezaras"
+        break
+        case 'de':
+        saveBtn.textContent = "Speichern"
+        settingsBtn.textContent = "-->Einstellungen"
+        closeBtn.textContent = "Schliesen"
+        break
+    }
+    
+
+}
+
+function getNewWeather(){
+    const inputCity = document.querySelector('#city')
+    console.log(inputCity)
+    if(inputCity.textContent === ''){
+        setTimeout(function(){inputCity.textContent = `Please add a location!`}, 3000)
+        
+
+   }else{
+    const lang = document.querySelector('.active').id
+    const city = document.querySelector('#city').value
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7d427ae79bf8f5a26954295a12ca5c14&lang=${lang}`
+    console.log(url)
+    fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => {
+        const obj = Object.values(data)
+        const wTemp = obj[3]
+        //console.log(obj)
+        //console.log(obj[3])
+
+        locationW.innerHTML = `${obj[11]}, ${obj[8].country}`
+        description.innerHTML = obj[1][0].description
+        wstring.innerHTML = `${Math.floor(wTemp.temp - 273.15)} ℃ , ${Math.floor(wTemp.temp * 9 / 5 - 459.67)} °F`
+        icon.src = `https://openweathermap.org/img/wn/${obj[1][0].icon}.png`
+        switch(lang){
+            case 'en':
+                humidity.innerHTML = `Relative Humidity: ${wTemp.humidity}% `
+                pressure.innerHTML = `Pressure: ${wTemp.pressure} hpa`
+                feelsLike.innerHTML = `Feels Like: ${Math.floor(wTemp.feels_like - 273.15)} ℃`
+                wind.innerHTML = ` Wind Speed: ${Math.floor(obj[5].speed * 3.6)} km/h`
+                break
+            case 'hu':
+                humidity.innerHTML = `Paratartalom: ${wTemp.humidity}% `
+                pressure.innerHTML = `Nyomas: ${wTemp.pressure} hpa`
+                feelsLike.innerHTML = `Valos Erzet: ${Math.floor(wTemp.feels_like - 273.15)} ℃`
+                wind.innerHTML = ` Szel Sebesseg: ${Math.floor(obj[5].speed * 3.6)} km/h` 
+                break
+            case 'de':
+                humidity.innerHTML = `Luftfeuchtigkeit: ${wTemp.humidity}% `
+                pressure.innerHTML = `Druck: ${wTemp.pressure} hpa`
+                feelsLike.innerHTML = `Wirkliches Gefühl: ${Math.floor(wTemp.feels_like - 273.15)} ℃`
+                wind.innerHTML = `Wind Geschwindigkeit: ${Math.floor(obj[5].speed * 3.6)} km/h`  
+                break  
+        }
+    })
+    }  
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+function getWeatherDefault () {
+    
+    const lang = document.querySelector('.active').id
+    const city = "Vienna"
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7d427ae79bf8f5a26954295a12ca5c14&lang=${lang}`
+    //console.log(url)
+    fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => {
+        const obj = Object.values(data)
+        const wTemp = obj[3]
+        //console.log(obj)
+        //console.log(obj[3])
+
+        locationW.innerHTML = `${obj[11]}, ${obj[8].country}`
+        description.innerHTML = obj[1][0].description
+        wstring.innerHTML = `${Math.floor(wTemp.temp - 273.15)} ℃ , ${Math.floor(wTemp.temp * 9 / 5 - 459.67)} °F`
+        icon.src = `https://openweathermap.org/img/wn/${obj[1][0].icon}.png`
+        humidity.innerHTML = `Relative Humidity: ${wTemp.humidity}% `
+        pressure.innerHTML = `Pressure: ${wTemp.pressure} hpa`
+        feelsLike.innerHTML = `Feels Like: ${Math.floor(wTemp.feels_like - 273.15)} ℃`
+        wind.innerHTML = ` Wind Speed: ${Math.floor(obj[5].speed * 3.6)} km/h`
+
+    })
+}
