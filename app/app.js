7b6b62e070521b100ba001969bfed5564c26c6e6
Andras Margittai <thunder6230@gmail.com>
2020-05-10 23:14:12 +0200

    Update app.js

--- app/app.js
+++ app/app.js
@@ -88,63 +88,71 @@ function setLang(){
 
 }
 
-function getNewWeather(){
+async function getNewWeather(){
+    
     const inputCity = document.querySelector('#city')
-    console.log(inputCity)
-    if(inputCity.textContent === ''){
-        setTimeout(function(){inputCity.textContent = `Please add a location!`}, 3000)
-        
-
-   }else{
     const lang = document.querySelector('.active').id
     const city = document.querySelector('#city').value
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7d427ae79bf8f5a26954295a12ca5c14&lang=${lang}`
-    console.log(url)
-    fetch(url)
-    .then((resp) => resp.json())
-    .then((data) => {
-        const obj = Object.values(data)
-        const wTemp = obj[3]
-        //console.log(obj)
-        //console.log(obj[3])
-
-        locationW.innerHTML = `${obj[11]}, ${obj[8].country}`
-        description.innerHTML = obj[1][0].description
-        wstring.innerHTML = `${Math.floor(wTemp.temp - 273.15)} ℃ , ${Math.floor(wTemp.temp * 9 / 5 - 459.67)} °F`
-        icon.src = `https://openweathermap.org/img/wn/${obj[1][0].icon}.png`
+    
+    const response = await fetch(url)
+    if(response.status !== 200){
         switch(lang){
             case 'en':
-                humidity.innerHTML = `Relative Humidity: ${wTemp.humidity}% `
-                pressure.innerHTML = `Pressure: ${wTemp.pressure} hpa`
-                feelsLike.innerHTML = `Feels Like: ${Math.floor(wTemp.feels_like - 273.15)} ℃`
-                wind.innerHTML = ` Wind Speed: ${Math.floor(obj[5].speed * 3.6)} km/h`
+                inputCity.value = `Please add an existing location!`
                 break
             case 'hu':
-                humidity.innerHTML = `Paratartalom: ${wTemp.humidity}% `
-                pressure.innerHTML = `Nyomas: ${wTemp.pressure} hpa`
-                feelsLike.innerHTML = `Valos Erzet: ${Math.floor(wTemp.feels_like - 273.15)} ℃`
-                wind.innerHTML = ` Szel Sebesseg: ${Math.floor(obj[5].speed * 3.6)} km/h` 
+                inputCity.value = `Kerem adjon meg egy letezo helyet!`
                 break
             case 'de':
-                humidity.innerHTML = `Luftfeuchtigkeit: ${wTemp.humidity}% `
-                pressure.innerHTML = `Druck: ${wTemp.pressure} hpa`
-                feelsLike.innerHTML = `Wirkliches Gefühl: ${Math.floor(wTemp.feels_like - 273.15)} ℃`
-                wind.innerHTML = `Wind Geschwindigkeit: ${Math.floor(obj[5].speed * 3.6)} km/h`  
-                break  
-        }
-    })
+                inputCity.value = `Bitte geben Sie einen existierenden Ort ein!`
+                break 
+        }          
+        inputCity.style.background = 'red'
+        inputCity.style.color = 'white'
+        inputCity.style.fontWeight = 'bold'
+        setTimeout(function(){
+            inputCity.value=""
+            inputCity.style.background = 'white'
+            inputCity.style.color = 'black'
+            inputCity.style.fontWeight = 'normal'
+        }, 2000)
+
+    }else{
+        const responseData = await response.json()
+    
+        console.log(responseData)
+            const wTemp = responseData.main
+            //console.log(obj)
+            //console.log(obj[3])
+
+            locationW.innerHTML = `${responseData.name}, ${responseData.sys.country}`
+            description.innerHTML = responseData.weather[0].description
+            wstring.innerHTML = `${Math.floor(wTemp.temp - 273.15)} ℃ , ${Math.floor(wTemp.temp * 9 / 5 - 459.67)} °F`
+            icon.src = `https://openweathermap.org/img/wn/${responseData.weather[0].icon}.png`
+            switch(lang){
+                case 'en':
+                    humidity.innerHTML = `Relative Humidity: ${wTemp.humidity}% `
+                    pressure.innerHTML = `Pressure: ${wTemp.pressure} hpa`
+                    feelsLike.innerHTML = `Feels Like: ${Math.floor(wTemp.feels_like - 273.15)} ℃`
+                    wind.innerHTML = ` Wind Speed: ${Math.floor(responseData.main.speed * 3.6)} km/h`
+                    break
+                case 'hu':
+                    humidity.innerHTML = `Paratartalom: ${wTemp.humidity}% `
+                    pressure.innerHTML = `Nyomas: ${wTemp.pressure} hpa`
+                    feelsLike.innerHTML = `Valos Erzet: ${Math.floor(wTemp.feels_like - 273.15)} ℃`
+                    wind.innerHTML = ` Szel Sebesseg: ${Math.floor(responseData.main.speed * 3.6)} km/h` 
+                    break
+                case 'de':
+                    humidity.innerHTML = `Luftfeuchtigkeit: ${wTemp.humidity}% `
+                    pressure.innerHTML = `Druck: ${wTemp.pressure} hpa`
+                    feelsLike.innerHTML = `Wirkliches Gefühl: ${Math.floor(wTemp.feels_like - 273.15)} ℃`
+                    wind.innerHTML = `Wind Geschwindigkeit: ${Math.floor(responseData.main.speed * 3.6)} km/h`  
+                    break  
+            }
+        
     }  
  }
-
-
-
-
-
-
-
-
-
-
 
 
 function getWeatherDefault () {
@@ -172,3 +180,16 @@ function getWeatherDefault () {
 
     })
 }
+
+document.querySelector('#city').addEventListener('keyup', async function() {
+    const city = document.querySelector('#city').value
+    console.log(city)
+   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7d427ae79bf8f5a26954295a12ca5c14`
+    
+    const response = await fetch(url)
+    if(response.status === 200){
+        saveBtn.setAttribute('data-dismiss', 'modal')
+    } else if( city === '' || response.status !== 200){
+        saveBtn.setAttribute('data-dismiss', 'none')
+    }
+})
